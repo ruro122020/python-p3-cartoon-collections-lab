b8257e16d8019d8c9065ee92b3c3506eb4a25df6
Ruth <ruro122020@gmail.com>
2024-01-15 22:00:03 -0500

    lab complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list):
+  for i, dwarf in enumerate(list):
+    print(f"{i+1}. {dwarf}")
+    
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+  return [f"{planeteer.capitalize()}!" for planeteer in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+  words = []
+  for word in list:
+    if len(word) > 4:
+      words.append(word)
+  return len(words) > 0
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+  cheeses = ["cheddar", "gouda", "camembert"]
+  for word in list:
+    if word in cheeses:
+      return word
+  
